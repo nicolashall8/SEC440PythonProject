9f12159cd825c6d1c073e68b632100f717923a69
nicolashall8 <71106483+nicolashall8@users.noreply.github.com>
2022-11-09 19:43:31 -0500

    added error handling

--- file_checksum.py
+++ file_checksum.py
@@ -12,24 +12,53 @@ def hash_menu():
         print(key, '-', hash_options[key])
 
 def sha_hash():
-    # Prompt the user to enter the full file path
-    filepath = input("Please enter the full path to your local file: ")
+   
+    try:
+        filepath = input("Please enter the full path to your file: ")
+        
+        hasher = hashlib.sha256()
+        with open(filepath,"rb") as open_file:
+            content = open_file.read()
+            hasher.update(content)
+        sha_output = hasher.hexdigest()
+        print("\n" + sha_output)
+
+        # Close the file
+        open_file.close()
     
-    hasher = hashlib.sha256()
-    with open(filepath,"rb") as open_file:
-        content = open_file.read()
-        hasher.update(content)
-    print(hasher.hexdigest())
+    except:
+        print("Error: Could not access file. Please provide the full file path...")
+
+        # Restart function
+        sha_hash()
+
+    else:
+        exit()
 
 def md5_hash():
-    # Prompt the user to enter the full file path
-    filepath = input("Please enter the full path to your local file: ")
+    
+    try:
+        filepath = input("Please enter the full path to your file: ")
+        
+        hasher = hashlib.md5()
+        with open(filepath,"rb") as open_file:
+            content = open_file.read()
+            hasher.update(content)
+        md5_output = hasher.hexdigest()
+        print("\n" + md5_output)
 
-    hasher = hashlib.md5()
-    with open(filepath,"rb") as open_file:
-        content = open_file.read()
-        hasher.update(content)
-    print(hasher.hexdigest())
+        # Close the file
+        open_file.close()
+    
+    except:
+        print("Error: Could not access file. Please provide the full file path...")
+
+        # Restart function
+        md5_hash()
+
+    else:
+        exit()
+    
 
 while(True):
     hash_menu()
@@ -38,9 +67,10 @@ while(True):
         user_choice = int(input("Select a menu option: "))
     except:
         print("\nInvalid input. Please enter a number... \n")
-    if user_choice == 1:
-        sha_hash()
-    elif user_choice == 2:
-        md5_hash()
-    elif user_choice == 3:
-        break
+    else:
+        if user_choice == 1:
+            sha_hash()
+        elif user_choice == 2:
+            md5_hash()
+        elif user_choice == 3:
+            break
